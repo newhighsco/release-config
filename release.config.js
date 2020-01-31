4217f47b623c36a90f30d150e9d29268bca0e800
Ben Edwards <hello@benedfit.com>
2020-01-31 16:42:29 +0000

    chore(docs): Adds CHANGELOG

--- release.config.js
+++ release.config.js
@@ -10,7 +10,12 @@ module.exports = {
       }
     ],
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     '@semantic-release/github',
-    '@semantic-release/npm'
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      { message: `chore(release): \${nextRelease.version} [skip ci]` }
+    ]
   ]
 }
